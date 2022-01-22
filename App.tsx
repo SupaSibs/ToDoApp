757a1089746820d7d828f88cefab894abd9f0699
SupaSibs <omareeto2012@hotmail.com>
2022-01-22 13:24:55 +0200

    first

--- App.tsx
+++ App.tsx
@@ -1,20 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react"
+import { StyleSheet, Text, View, TextInput, Image,} from 'react-native';
+import styles from "./styles";
+class App extends React.Component {
+constructor(props: any) {
+super(props);
+this.state = {
+Lists: 
+ }
+}
+render() {
+  return(
+<View style={styles.container}>
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+</View>
   );
 }
+}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
